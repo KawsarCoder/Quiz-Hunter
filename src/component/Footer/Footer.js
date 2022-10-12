65bdf33f53ef504a1160321dc124516888e7bbe9
KawsarCoder <96495318+KawsarCoder@users.noreply.github.com>
2022-10-12 11:51:19 +0600

    Footer added

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="bg-dark text-white p-5 mt-5">
+        <h6>@ 2022-2025; All reserved by Quiz Hunter</h6>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
